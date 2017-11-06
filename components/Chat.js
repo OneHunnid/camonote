169e041c6df63214fd195de5b79071e228eb6ae5
Chris Limbrick <chris_limbrick@ascapny-0018.fios-router.home>
2017-11-06 17:21:43 -0500

    WIP: loading chat messages. They are out of order, need to fix along with adding error/no messages state

--- components/Chat.js
+++ components/Chat.js
@@ -1,17 +1,32 @@
 import React from 'react'
+import _ from 'lodash'
+import $ from 'jquery'
 
 export default class Chat extends React.Component {
   constructor(props) {
     super(props)
   }
-  __createMessages() {
-    console.log(this.props.data)
+  __createMessagesList() {
+    const obj = this.props.data
+    const arrayByTime = _.sortBy(obj, 'timestamp');
+    const arrayReverse = _.reverse(arrayByTime)
+
+    _.forEach(arrayReverse, (value, key) => {
+      const message = value.message
+
+      $('#chatMessageList').append(
+        $('<div/>')
+        .addClass("chat-message")
+        .append("<span/>")
+        .text(message)
+      );
+    })
   }
   render() {
-    this.__createMessages()
+    this.__createMessagesList()
     return (
       <div className="">
-        Chat!
+        <div id="chatMessageList"></div>
       </div>
     )
   }
